2030f3935c91e4177ae928ca973c60f42490f87e
angela.brunst <angela.hairstylist@gmail.com>
2022-06-29 07:33:45 -0700

    Test oauth

--- server.js
+++ server.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { auth } = require('express-oauth2-jwt-bearer');
 
 const port = process.env.PORT || 8080;
 const app = express();
+
+const checkJwt = auth({
+    audience: 'YOUR_API_IDENTIFIER',
+    issuerBaseURL: `https://dev-cp5ml92r.us.auth0.com/`,
+});
+
+app.get('/api/public', function(req, res) {
+    res.json({
+        message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+    });
+});
+
+// This route needs authentication
+app.get('/api/private', checkJwt, function(req, res) {
+    res.json({
+        message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+    });
+});
+
+// const config = {
+//     authRequired: false,
+//     auth0Logout: true,
+//     secret: process.env.SECRET,
+//     baseURL: process.env.BASE_URL,
+//     clientID: process.env.CLIENT_ID,
+//     issuerBaseURL: process.env.ISSUER_BASE_URL,
+// };
+
+// app.use(auth(config));
+
+// app.get('/', (req, res) => {
+//     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
 
 app
     .use(bodyParser.json())
